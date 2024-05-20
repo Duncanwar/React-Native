8753bb5b661de2bb2ec61ff63f2b3cc1a6779a57
Duncanwar <semunda9@gmail.com>
2024-05-20 15:47:01 +0200

    register

--- hoop/App.js
+++ hoop/App.js
@@ -1,82 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import Register from "./screens/Register";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          fontFamily: "Ciutadella",
-          color: "white",
-          height: 80,
-          left: 32,
-          fontWeight: 400,
-          fontSize: 32,
-          lineHeight: 38.4,
-        }}
-      >
-        Glad to see you!!
-      </Text>
-      <StatusBar style="auto" />
-      <View style={styles.form}>
-        <TextInput
-          style={[styles.btnField, styles.email]}
-          placeholder="Email"
-        />
-        <TextInput
-          style={[styles.btnField, styles.password]}
-          placeholder="Password"
-        />
-        <TextInput
-          style={[styles.btnField, styles.password]}
-          placeholder="Password Authentication"
-        />
-        <TextInput
-          style={[styles.btnField, styles.password]}
-          placeholder="Phone Number"
-        />
-        <Button style={[styles.btnField,styles.button]} title="Register"  />
-        <Text>Forgot password? Retrieve</Text>
-      </View>
-    </View>
-  );
+export default function App(){
+  return(
+    <>
+    <Register />
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#2e2e99",
-    justifyContent: "flex-end",
-  },
-  form: {
-    backgroundColor: "#f4f4f4",
-    height: "80%",
-    width: "100%",
-    borderTopLeftRadius: 38,
-    borderTopRightRadius: 38,
-    paddingTop: 50,
-    alignItems: "center",
-  },
-  btnField: {
-    height: 60,
-    width: 311,
-    paddingLeft: 20,
-    borderRadius: 15,
-  },
-  email: {
-    backgroundColor: "#ffffff",
-  },
-  password: {
-    marginTop: 20,
-    backgroundColor: "#ffffff",
-  },
-  end:{
-    marginTop: 120,
-    marginBottom:120,
-    height: 60,
-    width: 311,
-  },
-  button:{
-    borderRadius: 15,
-  }
-});
---
+++ hoop/screens/Register.jsx
@@ -0,0 +1,128 @@
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import * as Font from "expo-font"
+import AppLoadding from "expo-app-loading"
+import MaterialCommunityIcons from "react-native-vector-icons"
+
+
+const loadFonts = async()=>{
+  await Font.loadAsync({
+    // 'Ciutadella': require('./assets/fonts/ciutadella.tff'),
+  });
+}
+
+export default function Register() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(()=>{
+    loadFonts().then(()=> setFontsLoaded(true));
+  }, [])
+  // if(!fontsLoaded){
+  //   return <AppLoadding />;
+  // }
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontFamily: "Ciutadella",
+          color: "white",
+          left: 32,
+          fontWeight: 400,
+          fontSize: 32,
+          lineHeight: 38.4,
+        }}
+      >
+        Let's start!!
+      </Text>
+      <StatusBar style="auto" />
+      <View style={styles.form}>
+        <TextInput
+          style={[styles.btnField, styles.email]}
+          placeholder="Email"
+          placeholderTextColor="rgba(45, 45, 45, 0.5)"
+        />
+        <TextInput
+          style={[styles.btnField, styles.password]}
+          placeholder="Password"
+          placeholderTextColor="rgba(45, 45, 45, 0.5)"
+        //   right={<TextInput.Icon 
+        //   name={()=><MaterialCommunityIcons name="eye-off" />} />
+        //   }
+        />
+        <TextInput
+          style={[styles.btnField, styles.password]}
+          placeholder="Password Authentication"
+          placeholderTextColor="rgba(45, 45, 45, 0.5)"
+        />
+        <TextInput
+          style={[styles.btnField, styles.password]}
+          placeholder="Phone Number"
+          placeholderTextColor="rgba(45, 45, 45, 0.5)"
+        />
+        <Pressable style={styles.button}>
+          <Text style={styles.text}>Register</Text>
+        </Pressable>
+        <Text style={{
+          paddingTop: 20,
+          color:"rgba(45, 45, 45, 0.5)",
+        }}>
+          <Text>Have an account?</Text>
+           <Text style={{
+            color:"red"
+           }}> Sign In</Text>
+          </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#130F26",
+    justifyContent: "flex-end",
+  },
+  form: {
+    marginTop:80,
+    backgroundColor: "#F4F4FA",
+    height: "80%",
+    width: "100%",
+    borderTopLeftRadius: 38,
+    borderTopRightRadius: 38,
+    paddingTop: 50,
+    alignItems: "center",
+  },
+  btnField: {
+    height: 60,
+    width: 311,
+    paddingLeft: 20,
+    borderRadius: 15,
+  },
+  email: {
+    backgroundColor: "#ffffff",
+  },
+  password: {
+    marginTop: 20,
+    backgroundColor: "#ffffff",
+  },
+  button:{
+    alignItems:'center',
+    justifyContent: 'center',
+    paddingVertical:12,
+    paddingHorizontal:32,
+    borderRadius: 15,
+    elevation:3,
+    backgroundColor:'#130F26',
+    height: 60,
+    width: 311,
+    marginTop: 110,
+  },
+  text:{
+    fontSize:16,
+    lineHeight:21,
+    fontWeight:'bold',
+    letterSpacing:0.25,
+    color:'white',
+  }
+});
