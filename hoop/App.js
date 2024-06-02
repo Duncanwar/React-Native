c7654cd2a4e209696d49f2d4c593db91f674dd37
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-06-02 23:40:24 +0200

    new

--- hoop/App.js
+++ hoop/App.js
@@ -11,9 +11,7 @@ export default function App(){
   return(
     <>
     <NavigationContainer>
-      <Stack.Navigator 
-      screenOptions={{headerShown: false}}
-      >
+      <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen
         name="Register"
         component={Register}
