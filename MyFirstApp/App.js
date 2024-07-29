da4cc3fed81e8ecb9a221e2d45477b0bd529255d
Duncanwar <semunda9@gmail.com>
2024-07-29 18:14:34 +0200

    new

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,14 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [text,setText] = useState('Music is good to the soul')
+  const [text, setText] = useState("Music is good to the soul");
+let x;
+x.toString()
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
+      <Text>{text} Hello!kjfkjdkjajdsjk
+      </Text>
       <StatusBar style="auto" />
-      <Button title="Change"  onPress={()=>{setText('I love to sing')}}/>
+      <Button
+        title="Change"
+        onPress={() => {
+          setText("I love to sing");
+        }}
+      />
     </View>
   );
 }
@@ -16,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
