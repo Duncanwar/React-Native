fb08732a54fd5c2b49d180e7b5f3bf865d000670
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-08-04 17:29:13 +0200

    flexbox

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -10,15 +10,30 @@ import {
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row" }}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 100,
+          height: 300,
         }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 200,
+          alignSelf: "flex-start",
+        }}
+      />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
