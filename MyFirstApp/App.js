54e1ff4fd2decffe832f5a078e44fc476ca9e80c
Duncanwar <semunda9@gmail.com>
2024-08-06 10:47:04 +0200

    encapsulating text

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,6 +1,6 @@
 import { View } from "react-native";
-import AppText from "./app/components/AppText";
 
+import AppText from "./app/components/AppText";
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
