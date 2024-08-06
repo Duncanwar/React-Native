41012ba7ca11f4e742a5cdfd6b4e9fbbba0ea203
Duncanwar <semunda9@gmail.com>
2024-08-06 12:31:50 +0200

    organising styles

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 
 import AppText from "./app/components/AppText";
+
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
