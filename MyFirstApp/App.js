b3e68c705b7bbeff33443463507afefd9eecf5c9
Duncanwar <semunda9@gmail.com>
2024-08-06 10:33:08 +0200

    styling text

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,30 +1,23 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View
+      <Text
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          padding: 20,
-          paddingHorizontal: 10,
-          paddingLeft: 30,
+          fontSize: 30,
+          fontStyle: "italic",
+          fontWeight: "600",
+          color: "tomato",
+          textTransform: "capitalize",
+          lineHeight: 30,
         }}
       >
-        <View style={{ backgroundColor: "gold", width: 50, height: 50 }}></View>
-      </View>
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-          margin: 20,
-        }}
-      ></View>
+        I love React Native! This is my first React Native app! Here some more
+        Text
+      </Text>
     </View>
   );
 }
