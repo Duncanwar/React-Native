eebdc98a6ea7e9f77be21e226ba384ae110a8988
Duncanwar <semunda9@gmail.com>
2024-08-06 10:50:36 +0200

    icons

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,10 +1,11 @@
 import { View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AppText from "./app/components/AppText";
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <AppText>I love React Native</AppText>
+      <MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
     </View>
   );
 }
