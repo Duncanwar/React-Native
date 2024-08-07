92cf197db5f1b19e88c3aa63bdd5344127fa8e23
Duncanwar <semunda9@gmail.com>
2024-08-07 16:12:30 +0200

    Extracting screen component

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,22 +1,5 @@
-import { View } from "react-native";
-
-import AppText from "./app/components/AppText";
-import AppButton from "./app/components/AppButton";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
   return <MessagesScreen />;
-}
-// <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
-{
-  /* <Card
-      title="Red jacket for sale"
-      subTitle="$100"
-      image={require("./app/assets/jacket.jpg")}
-    />
-  </View> */
 }
