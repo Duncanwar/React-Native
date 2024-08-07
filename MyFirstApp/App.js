e1591987e553e39f0d3bd62e2fb825298ed1e12e
Duncanwar <semunda9@gmail.com>
2024-08-07 15:25:41 +0200

    Improving the view Image screen

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -5,9 +5,10 @@ import AppButton from "./app/components/AppButton";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
 // <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
 {
