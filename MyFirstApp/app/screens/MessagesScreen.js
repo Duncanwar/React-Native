04328ef43b6e72803d4571e27d31f7fd5d4ce596
Duncanwar <semunda9@gmail.com>
2024-08-08 10:13:32 +0200

    Implementing Pull to Refresh

--- MyFirstApp/app/screens/MessagesScreen.js
+++ MyFirstApp/app/screens/MessagesScreen.js
@@ -22,6 +22,8 @@ const initialMessages = [
 ];
 function MessageScreen() {
   const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
   const handleDelete = (message) => {
     // Delete the message from message array
     setMessages(messages.filter((m) => m.id !== message.id));
@@ -43,6 +45,17 @@ function MessageScreen() {
           />
         )}
         ItemSeparatorComponent={<ListItemSeparator />}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/mosh.jpg"),
+            },
+          ]);
+        }}
       />
     </Screen>
   );
