26f13c5e6aaaefcdd18e5f8363d914d20c32d3de
Duncanwar <semunda9@gmail.com>
2024-08-09 15:52:52 +0200

    Text Input

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,4 +1,7 @@
+import { Text, TextInput } from "react-native";
+import { useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import Screen from "./app/components/Screen";
@@ -8,9 +11,17 @@ import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
+  const [firstName, setFirstName] = useState("");
   return (
-    <GestureHandlerRootView>
-      <ListingsScreen />
-    </GestureHandlerRootView>
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        secureTextEntry={true}
+        keyboardType="numeric"
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{ borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+      />
+    </Screen>
   );
 }
