747f375c3e685b1cd1cea7a980d87357b1fa7c98
Duncanwar <semunda9@gmail.com>
2024-08-09 17:02:50 +0200

    switch

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,23 +1,13 @@
-import { Text, TextInput } from "react-native";
+import { Switch } from "react-native";
 import { useState } from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
 import Screen from "./app/components/Screen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppText from "./app/components/AppText";
-import AppTextInput from "./app/components/AppTextInput";
-import colors from "./app/config/colors";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
+  const [isNew, setIsNew] = useState(false);
   return (
     <Screen>
-      <AppTextInput placeholder="Username" icon="email" />
+      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
     </Screen>
   );
 }
