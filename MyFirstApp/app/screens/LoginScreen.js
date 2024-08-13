5f48bc798222ec52a0d99af8e459b96a2082c435
Duncanwar <semunda9@gmail.com>
2024-08-13 12:21:10 +0200

    Form validation with yup

--- MyFirstApp/app/screens/LoginScreen.js
+++ MyFirstApp/app/screens/LoginScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Image, StyleSheet } from "react-native";
 import { Formik } from "formik";
+import * as Yup from "yup";
 
 import Screen from "../components/Screen";
+import AppText from "../components/AppText";
 import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 function LoginScreen(props) {
   return (
@@ -13,27 +20,30 @@ function LoginScreen(props) {
       <Formik
         initialValues={{ email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit }) => (
+        {({ handleChange, handleSubmit, errors }) => (
           <>
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
               icon="email"
               keyboardType="email-address"
-              onChangeText={handleChange(email)}
+              onChangeText={handleChange("email")}
               placeholder="Email"
               textContentType="emailAddress"
             />
+            <AppText style={{ color: "red" }}>{errors.email}</AppText>
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
               icon="lock"
-              onChangeText={handleChange(password)}
+              onChangeText={handleChange("password")}
               placeholder="password"
               secureTextEntry={true}
               textContentType="password"
             />
+            <AppText style={{ color: "red" }}>{errors.password}</AppText>
             <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}
