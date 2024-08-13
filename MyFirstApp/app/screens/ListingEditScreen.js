7b27b4eefb52dbaae74c70f9bd854226fad2dcd8
Duncanwar <semunda9@gmail.com>
2024-08-13 17:08:59 +0200

    Register screen

---
+++ MyFirstApp/app/screens/ListingEditScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { first } from 'react-native'
+import Screen from '../components/Screen';
+function ListingEditScreen(props) {
+    return (
+        <Screen>
+            <Ap
+        </Screen>
+    );
+}
+const styles = StyleSheet.create({
+    
+})
+export default ListingEditScreen;
--- MyFirstApp/app/screens/RegisterScreen.js
+++ MyFirstApp/app/screens/RegisterScreen.js
@@ -2,30 +2,36 @@ import React from "react";
 import { Image, StyleSheet } from "react-native";
 import * as Yup from "yup";
 
-import Screen from "../components/Screen";
 import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+import Screen from "../components/Screen";
 
 const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
   email: Yup.string().required().email().label("Email"),
   password: Yup.string().required().min(4).label("Password"),
 });
 
-function LoginScreen(props) {
+function RegisterScreen(props) {
   return (
     <Screen style={styles.screen}>
-      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
       <AppForm
-        initialValues={{ email: "", password: "" }}
+        initialValues={{ name: "", email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
         <AppFormField
-          autoCapitalize="none"
+          autoCorrect={false}
+          icon="account"
+          name="name"
+          placeholder="Name"
+        />
+        <AppFormField
+          autoCapitalize={false}
           autoCorrect={false}
           icon="email"
           keyboardType="email-address"
           name="email"
-          placeholder="Email"
+          placeholder="email"
           textContentType="emailAddress"
         />
         <AppFormField
@@ -33,22 +39,18 @@ function LoginScreen(props) {
           autoCorrect={false}
           icon="lock"
           name="password"
-          placeholder="password"
-          secureTextEntry={true}
+          placeholder="Password"
+          secureTextEntry
           textContentType="password"
         />
-        <SubmitButton title="Login" />
+        <SubmitButton title="Register" />
       </AppForm>
     </Screen>
   );
 }
 const styles = StyleSheet.create({
-  logo: {
-    width: 80,
-    height: 80,
-    alignSelf: "center",
-    marginTop: 50,
-    marginBottom: 20,
+  screen: {
+    marginTop: 10,
   },
 });
-export default LoginScreen;
+export default RegisterScreen;
