64e16265b75ae14039c5a45a1e10f6c5809bc47d
Duncanwar <semunda9@gmail.com>
2024-08-14 14:25:16 +0200

    handling text

--- MyFirstApp/app/components/AppText.js
+++ MyFirstApp/app/components/AppText.js
@@ -3,9 +3,9 @@ import { Text } from "react-native";
 
 import defaultStyles from "../config/styles";
 
-function AppText({ children, style }) {
+function AppText({ children, style, ...otherProps }) {
   return (
-    <Text numberOfLines={4} style={[defaultStyles.text, style]}>
+    <Text style={[defaultStyles.text, style]} {...otherProps}>
       {children}
     </Text>
   );
