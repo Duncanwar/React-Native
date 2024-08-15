2577f1f80eb01f7a47441384d8cb8ad971714b78
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-08-15 13:40:35 +0200

    assessing library

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,15 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Button, Image } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
   const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync(
+      Permissions.CAMERA_ROLL,
+      Permissions.LOCATION
+    );
     if (!granted) alert("You need to enable permissions to access the camera");
   };
   useEffect(() => {
     requestPermission();
   }, []);
-  return <Screen />;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an imaage", error);
+    }
+  };
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
