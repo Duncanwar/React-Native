ace0538a62a22af26bf31626c10c3758b0361cc1
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-08-17 23:56:59 +0200

    new

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -2,11 +2,5 @@ import Screen from "./app/components/Screen";
 import ListingEditingScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return (
-    // <Screen>
-    <ListingEditingScreen />
-  );
-}
-{
-  /* </Screen> */
+  return <ListingEditingScreen />;
 }
