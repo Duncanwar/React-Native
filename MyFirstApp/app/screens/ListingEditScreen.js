e4a352d88b0aa9cfbcd73af42c54c4408b0e403e
Duncanwar <semunda9@gmail.com>
2024-08-23 15:22:32 +0200

    resetform

--- MyFirstApp/app/screens/ListingEditScreen.js
+++ MyFirstApp/app/screens/ListingEditScreen.js
@@ -85,7 +85,7 @@ function ListingEditingScreen(props) {
   const [uploadVisible, setUploadVisible] = useState(false);
   const [progress, setProgress] = useState(0);
 
-  const handleSubmit = async (listing) => {
+  const handleSubmit = async (listing, { resetForm }) => {
     setProgress(0);
     setUploadVisible(true);
     const result = await listingsApi.addListing(
@@ -97,7 +97,7 @@ function ListingEditingScreen(props) {
       setUploadVisible(false);
       return alert("Could not save the listing.");
     }
-    alert("Success");
+    resetForm();
   };
   return (
     <Screen style={styles.screen}>
