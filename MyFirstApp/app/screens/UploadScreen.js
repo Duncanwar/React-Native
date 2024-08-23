081b2b8e197904c6805df71e1803b5e475920c5e
Duncanwar <semunda9@gmail.com>
2024-08-23 15:01:31 +0200

    Building the upload screen

--- MyFirstApp/app/screens/UploadScreen.js
+++ MyFirstApp/app/screens/UploadScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Modal, StyleSheet, View } from "react-native";
-import Text from "../components/AppText";
+import { Bar } from "react-native-progress";
+
+import colors from "../config/colors";
 
 function UploadScreen({ progress = 0, visible = false }) {
   return (
     <Modal visible={visible}>
       <View style={styles.container}>
-        <Text>{progress * 100}%</Text>
+        <Bar color={colors.primary} progress={progress} width={200} />
       </View>
     </Modal>
   );
