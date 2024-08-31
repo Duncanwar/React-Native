9ff942c8a644a869aca1211e8d34deec5cb82534
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-08-31 20:10:08 +0200

    new

--- myfirstapp/Backend/routes/auth.js
+++ myfirstapp/Backend/routes/auth.js
@@ -13,6 +13,7 @@ const schema = {
 router.post("/", validateWith(schema), (req, res) => {
   const { email, password } = req.body;
   const user = usersStore.getUserByEmail(email);
+  console.log(user);
   if (!user || user.password !== password)
     return res.status(400).send({ error: "Invalid email or password." });
 
